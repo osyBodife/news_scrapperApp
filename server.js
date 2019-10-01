e54b0a9c47b799000c32a1237cba559b93cc8791
Osy odife <osyodife@gmail.com>
2019-09-30 22:25:12 -0400

    upload current changes to files and folders

--- server.js
+++ server.js
@@ -7,34 +7,52 @@ var logger = require("morgan");
 var express = require("express");
 var app = express();
 
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+// Require all models
+//var db = require("./models");
+var PORT = 3000;
+
+// Configure middleware
+// Use morgan logger for logging requests
 app.use(logger("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 
-app.use(express.static(process.cwd() + "/public"));
+// Make public a static folder
+app.use(express.static("public"));
+
 //set up handlebars
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//connecting to MongoDB
-//mongoose.connect("mongodb://localhost/scraped_news");
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/scrapperApp", { useNewUrlParser: true });
+
 const MONGODB_URI =
-    process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+    process.env.MONGODB_URI || "mongodb://localhost/scrapperApp";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-    console.log("Connected to Mongoose!");
-});
+// var db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function () {
+//     console.log("Connected to Mongoose!");
+// });
 
-var routes = require("./controller/controller.js");
-app.use("/", routes);
+var routes = require("./controller/app.js");
+//app.use("/", routes);
 //Create localhost port
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log("Listening on PORT " + port);
+var PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
 });
-© 2019 GitHub, Inc.
