fc53baf763257d810778691e63d504596361aa6f
Osy odife <osyodife@gmail.com>
2019-10-06 18:31:56 -0400

    update changes to files and folders

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -26,6 +27,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+
+
+//set up handlebars
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/scraperApp", { useNewUrlParser: true });
@@ -79,100 +86,29 @@ app.get("/scrape", function (req, res) {
     });
 });
 
-// var cheerio = require("cheerio");
-// var axios = require("axios");
-
-// // First, tell the console what server2.js is doing
-// console.log("\n******************************************\n" +
-//     "Grabbing every article headline and link\n" +
-//     "from the NHL website:" +
-//     "\n******************************************\n");
-
-// // Making a request via axios for `nhl.com`'s homepage
-// //axios.get("https://www.nhl.com/").then(function (response) {
-// // Making a request via axios for `nhl.com`'s homepage
-// axios.get("https://www.latimes.com/").then(function (response) {
-
-//     // Load the body of the HTML into cheerio
-//     var $ = cheerio.load(response.data);
-
-//     // Empty array to save our scraped data
-//     var results = [];
-
-//     // With cheerio, find each h4-tag with the class "headline-link" and loop through the results
-    
-//     $(".PromoSmall-title").each(function (i, element) {
-        
-//             // Save an empty result object
-//             var result = {};
-//             // Add the text and href of every link, and save them as properties of the result object
-//             result.title = $(this)
-//                 .children("a")
-//                 .text();
-//             result.link = $(this)
-//                 .children("a")
-//                 .attr("href");
-//         // var headline = $(this).children("a").text().trim();
-
-//         // var storylink = $(this).children("a").attr("href");
-
-//         // // Make an object with data we scraped for this h4 and push it to the results array
-//         // results.push({
-//         //     title: headline,
-//         //     link: storylink
-//         // });
-
-//             // Create a new Article using the `result` object built from scraping
-//             db.Article.create(result)
-//                 .then(function (dbArticle) {
-//                     // View the added result in the console
-//                     console.log("From Db : " + dbArticle);
-//                 })
-//                 .catch(function (err) {
-//                     // If an error occurred, log it
-//                     console.log(err);
-//                 });
-//         });
-
-// //         // Send a message to the client
-// //         res.send("Scrape Complete");
-// //     });
-
-//     console.log(results);
-
-//     });
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+           //res.json(dbArticle);
+            res.render("index", {article: dbArticle});
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 
-
-// // Route for getting all Articles from the db
-// app.get("/articles", function (req, res) {
-//     // Grab every document in the Articles collection
-//     db.Article.find({})
-//         .then(function (dbArticle) {
-//             // If we were able to successfully find Articles, send them back to the client
-//             res.json(dbArticle);
-//         })
-//         .catch(function (err) {
-//             // If an error occurred, send it to the client
-//             res.json(err);
-//         });
-// });
-
-// // Route for grabbing a specific Article by id, populate it with it's note
-// app.get("/articles/:id", function (req, res) {
-//     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//     db.Article.findOne({ _id: req.params.id })
-//         // ..and populate all of the notes associated with it
-//         .populate("note")
-//         .then(function (dbArticle) {
-//             // If we were able to successfully find an Article with the given id, send it back to the client
-//             res.json(dbArticle);
-//         })
-//         .catch(function (err) {
-//             // If an error occurred, send it to the client
-//             res.json(err);
-//         });
-// });
+app.get("/readArticle/:id", function (req, res) {
+    var articleId = req.params.id;
+    var hbsObj = {
+        article: [],
+        body: []
+    };
+});
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
@@ -194,7 +130,8 @@ app.post("/articles/:id", function (req, res) {
         });
 });
 
-// Start the server
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
-});
+
+    // Start the server
+    app.listen(PORT, function () {
+        console.log("App running on port " + PORT + "!");
+    });
