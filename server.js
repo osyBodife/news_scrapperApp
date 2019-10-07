b5ffc563606c8de97932455ba88dff9d6a28d457
Osy odife <osyodife@gmail.com>
2019-10-06 20:42:59 -0400

    upload recent changes to files and folders

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
+var path = require("path");
+
+var request = require("request");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -102,23 +105,34 @@ app.get("/articles", function (req, res) {
 });
 
 
-app.get("/readArticle/:id", function (req, res) {
-    var articleId = req.params.id;
-    var hbsObj = {
-        article: [],
-        body: []
-    };
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate("comment")
+        .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            //res.json(dbArticle);
+
+            res.render("articles", { article: dbArticle });
+
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
 });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
     // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-        .then(function (dbNote) {
+    db.Comment.create(req.body)
+        .then(function (dbComment) {
             // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
             // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
         })
         .then(function (dbArticle) {
             // If we were able to successfully update an Article, send it back to the client
@@ -129,6 +143,16 @@ app.post("/articles/:id", function (req, res) {
             res.json(err);
         });
 });
+
+
+
+
+
+
+
+
+
+
 
 
     // Start the server
